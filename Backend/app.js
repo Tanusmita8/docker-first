2d30a8ad3e847052b7a3a62e0593c3ca4cc1cb84
Tanusmita8 <tanusmitadas106@gmail.com>
2026-02-06 14:47:15 +0530

    Fix Render PORT issue

--- Backend/app.js
+++ Backend/app.js
@@ -6,6 +6,7 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(5000, () => {
-  console.log("Backend running on port 5000");
+
+app.listen(PORT, () => {
+  console.log(`Backend running on port ${PORT}`);
 });
