76d5868392f31f1f5ffe3cc93bb769e2f3ad25ea
Tanusmita8 <tanusmitadas106@gmail.com>
2026-02-06 14:41:25 +0530

    Fix port and clarify API routes

--- Backend/app.js
+++ Backend/app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
 
-app.get("/api", (req, res) => {
+app.get("/", (req, res) => {
   res.json({ message: "Hello from BACKEND container" });
 });
 
+const PORT = process.env.PORT || 5000;
 app.listen(5000, () => {
   console.log("Backend running on port 5000");
 });
